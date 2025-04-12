9e5e4cea6e1b6a1c6caff373ef86a7b88fb40165
Himanshi <himanshi.shende619@gmail.com>
2025-04-12 10:13:18 +0530

    A bit more change

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://chat-app-v3-backend.onrender.com/" : "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://chat-app-v3-backend.onrender.com" : "/api",
   withCredentials: true,
 });
